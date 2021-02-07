dbae24d32afa2443d6d367871d6940c410e932fc
Robin Muller <robin.muller.romutech@gmail.com>
2021-02-07 14:05:22 +0100

    feature: creation of the home model

--- cms/models.py
+++ cms/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Home(models.Model):
+    logo = models.ImageField(upload_to='logo')
+    picture = models.ImageField(upload_to='picture')
+    date = models.DateTimeField(default=timezone.now)
+    
