c7466e0388b591d7ae284665fb21e60418b7d831
Robin Muller <robin.muller.romutech@gmail.com>
2021-02-07 16:14:55 +0100

    feature: dynamization of the template

--- cms/views.py
+++ cms/views.py
@@ -1,4 +1,12 @@
 from django.shortcuts import render
+from cms.models import Home, Features, Application, Api, Documentation, Footer
 
 def index(request):
+    home            = Home.objects.latest('id')
+    features        = Features.objects.latest('id')
+    application     = Application.objects.latest('id')
+    api             = Api.objects.latest('id')
+    documentation   = Documentation.objects.latest('id')
+    footer          = Footer.objects.latest('id')
+
     return render(request, 'cms/index.html', locals())
