160dd44bf6fddbb48a26b0af1fd0c9348b88cce2
Robin Muller <robin.muller.romutech@gmail.com>
2021-02-07 14:12:20 +0100

    feature: creation of the application model

--- cms/models.py
+++ cms/models.py
@@ -2,7 +2,17 @@ from django.db import models
 from django.utils import timezone
 
 class Home(models.Model):
-    logo = models.ImageField(upload_to='logo')
+    logo    = models.ImageField(upload_to='logo')
     picture = models.ImageField(upload_to='picture')
-    date = models.DateTimeField(default=timezone.now)
-    
+    date    = models.DateTimeField(default=timezone.now)
+
+
+class Features(models.Model):
+    titre       = models.CharField(max_length=200)
+    description = models.TextField()
+    features    = models.JSONField(null=True)
+
+
+class Application(models.Model):
+    titre   = models.CharField(max_length=200)
+    button  = models.CharField(max_length=50)
