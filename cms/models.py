3e4df5a55e086b4e03e47c8594aa680bbf72c9c9
Robin Muller <robin.muller.romutech@gmail.com>
2021-02-07 21:11:04 +0100

    chore: refactoring of the index view

--- cms/models.py
+++ cms/models.py
@@ -1,33 +1,52 @@
 from django.db import models
 from django.utils import timezone
 
+
 class Home(models.Model):
-    logo    = models.ImageField(upload_to='logo')
+    logo = models.ImageField(upload_to='logo')
     picture = models.ImageField(upload_to='picture')
-    date    = models.DateTimeField(default=timezone.now)
+    date = models.DateTimeField(default=timezone.now)
+
+    class Meta:
+        verbose_name = "home"
 
 
 class Features(models.Model):
-    titre       = models.CharField(max_length=200)
+    title = models.CharField(max_length=200)
     description = models.TextField()
-    features    = models.JSONField(null=True)
+    features = models.JSONField(null=True)
+
+    class Meta:
+        verbose_name = "features"
 
 
 class Application(models.Model):
-    titre   = models.CharField(max_length=200)
-    button  = models.CharField(max_length=50)
+    title = models.CharField(max_length=200)
+    button = models.CharField(max_length=50)
+
+    class Meta:
+        verbose_name = "application"
 
 
 class Api(models.Model):
-    titre = models.CharField(max_length=200)
+    title = models.CharField(max_length=200)
     description = models.TextField()
+
+    class Meta:
+        verbose_name = "api"
 
 
 class Documentation(models.Model):
-    titre       = models.CharField(max_length=200)
+    title = models.CharField(max_length=200)
     description = models.TextField()
-    button      = models.CharField(max_length=50)
+    button = models.CharField(max_length=50)
+
+    class Meta:
+        verbose_name = "documentation"
 
 
 class Footer(models.Model):
     content = models.TextField()
+
+    class Meta:
+        verbose_name = "footer"
--- cms/views.py
+++ cms/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
 from cms.models import Home, Features, Application, Api, Documentation, Footer
 
-def index(request):
-    home            = Home.objects.latest('id')
-    features        = Features.objects.latest('id')
-    application     = Application.objects.latest('id')
-    api             = Api.objects.latest('id')
-    documentation   = Documentation.objects.latest('id')
-    footer          = Footer.objects.latest('id')
 
-    return render(request, 'cms/index.html', locals())
+def index(request):
+    model_list = [Home, Features, Application, Api, Documentation, Footer]
+    data = {}
+
+    for model in model_list:
+        try:
+            data[model._meta.verbose_name] = model.objects.latest('id')
+        except:
+            pass
+
+    return render(request, 'cms/index.html', data)
