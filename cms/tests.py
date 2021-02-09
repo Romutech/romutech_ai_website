10f2569fe9b80fe7507e32d75e33913bdeb0fd26
Robin Muller <robin.muller.romutech@gmail.com>
2021-02-09 22:26:51 +0100

    chore: creation of unit tests for the application model

--- cms/tests.py
+++ cms/tests.py
@@ -95,3 +95,72 @@ class PresentationTest(TestCase):
 
         self.assertEquals(self.model.objects.count(), 0)
 
+
+#*******************************************************************************
+#                             Application Tests                                #
+#*******************************************************************************
+
+class ApplicationTest(TestCase):
+    def setUp(self):
+        self.model = Application
+
+        self.title = "Lancer L'application Romutech AI"
+        self.button = "Romutech AI"
+
+        self.bad_title = "Romutech AI"
+        self.wrong_button = "Wrong butto,n"
+
+
+    def test_create_application(self):
+        self.assertEquals(Application.objects.count(), 0)
+
+        application = self.model.objects.create(title=self.title, button=self.button)
+
+        self.assertEquals(Application.objects.count(), 1)
+        self.assertEquals(application.title, self.title)
+        self.assertEquals(application.button, self.button)
+        self.assertNotEquals(application.title, self.bad_title)
+        self.assertNotEquals(application.button, self.wrong_button)
+
+
+    def test_read_application(self):
+        self.model.objects.create(title=self.title, button=self.button)
+
+        self.assertEquals(self.model.objects.count(), 1)
+
+        application = self.model.objects.latest('id')
+
+        self.assertEquals(application.title, self.title)
+        self.assertEquals(application.button, self.button)
+        self.assertNotEquals(application.title, self.bad_title)
+        self.assertNotEquals(application.button, self.wrong_button)
+
+
+    def presentation(self):
+        self.model.objects.create(title=self.title, button=self.button)
+
+        self.assertEquals(self.model.objects.count(), 1)
+
+        application = self.model.objects.latest('id')
+
+        self.assertEquals(application.title, self.title)
+        self.assertEquals(application.button, self.button)
+
+        application.title = "New title"
+        application.save(update_fields=['title'])
+
+        self.assertEquals(application.title, "New title")
+        self.assertEquals(application.butto, self.button)
+
+
+    def test_delete_feature(self):
+        self.model.objects.create(title=self.title, button=self.button)
+
+        self.assertEquals(self.model.objects.count(), 1)
+
+        application = self.model.objects.latest('id')
+
+        application.delete()
+
+        self.assertEquals(self.model.objects.count(), 0)
+    
