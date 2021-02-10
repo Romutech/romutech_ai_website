6e03e81ffd8d4c1671736e964330b1e9832f0ccc
Robin Muller <robin.muller.romutech@gmail.com>
2021-02-10 01:31:35 +0100

    chore: creation of the unit tests for the views test

--- cms/tests.py
+++ cms/tests.py
@@ -366,3 +366,57 @@ class FooterTest(TestCase):
         footer.delete()
 
         self.assertEquals(self.model.objects.count(), 0)
+
+
+# **************************************************************************** #
+#                                Views test                                    #
+# **************************************************************************** #
+
+class ViewsTest(TestCase):
+    def setUp(self):
+        settings.APPEND_SLASH = False
+        self.url = reverse('index')
+
+        Presentation.objects.create(
+            title = "Romutech AI",
+            description = "Romutech AI est une solution basée sur l'intelligence artificielle.",
+            features = [
+                {
+                    "title": "Détection de visage",
+                    "content": "La solution permet de detecter des visages."
+                },
+                {
+                    "title": "Floutage des visages.",
+                    "content": "La solution permet d'automatiser le floutage des visages sur une photo."
+                }
+            ]
+        )
+        Application.objects.create(title="Lancer L'application Romutech AI", button="Romutech AI")
+        Api.objects.create(title="L'API", description="Une API est mise à disponibilité des développeurs.")
+        Documentation.objects.create(
+            title = "Documentation de l'API",
+            description = "Une documentation est disponible. Vous y trouverez les différentes routes existantes, etc.",
+            button = "Documentation"
+        )
+        Footer.objects.create(content="© Romutech | 2021")
+
+    def test_index_view(self):
+        response = self.client.get(self.url)
+
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response, 'cms/index.html')
+        self.assertTemplateUsed(response, 'base.html')
+        self.assertTemplateNotUsed(response, 'menu.html')
+        self.assertTemplateNotUsed(response, '404.html')
+        self.assertTemplateNotUsed(response, '500.html')
+
+
+    def test_404_view(self):
+        response = self.client.get("page_that_does_not_exist")
+
+        self.assertEquals(response.status_code, 404)
+        self.assertTemplateNotUsed(response, 'cms/index.html')
+        self.assertTemplateUsed(response, 'base.html')
+        self.assertTemplateUsed(response, 'menu.html')
+        self.assertTemplateUsed(response, '404.html')
+        self.assertTemplateNotUsed(response, '500.html')
