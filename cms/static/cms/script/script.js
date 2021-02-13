345b3aef5d02a068ab3e606da34a51b8c9211891
Robin Muller <robin.muller.romutech@gmail.com>
2021-02-13 01:24:57 +0100

    feature: replacement of the popup by a modal

--- cms/static/cms/script/script.js
+++ cms/static/cms/script/script.js
@@ -1,28 +1,13 @@
-// Pop up
-$(function(){
-    $(".popup-light").click(function() {
-        var obj = $(this),
-            popupClass = obj.data("popupClass"),
-            popupWidth = obj.data("popupWidth"),
-            objPopup =  $('.' + popupClass);
-
-        objPopup
-            .css("width", popupWidth)
-            .prepend('<img src="static/cms/images/close.png" class="popup-btn-close" title="Close Window" alt="Close" />')
-            .css({
-                "margin-top":  -objPopup.outerHeight(true)/2,
-                "margin-left": -objPopup.outerWidth(true)/2
-            })
-            .fadeIn();
-
-        return false;
+// modale
+$(document).ready(function() {
+    $("#modale_open").click(function() {
+        $(".modale").fadeIn();
+        $(".message").slideDown();
     });
 
-    $("body").delegate(".popup-btn-close", "click", function(){
-        $('.popup-block').fadeOut(function(){
-            $(".popup-btn-close").remove();
-        });
-
-        return false;
+    $("#modale_close").click(function() {
+        $(".message").slideUp();
+        $(".modale").fadeOut();
     });
 });
+
