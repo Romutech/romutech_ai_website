cfadb8a27a108d3588d58d3ce22e41eed5fe6c82
Robin Muller <robin.muller.romutech@gmail.com>
2021-02-13 02:06:58 +0100

    chore: minification of css and javascript files as well as refactoring of javascript

--- cms/static/cms/script/script.js
+++ cms/static/cms/script/script.js
@@ -1,3 +1,29 @@
+$(document).ready(function() {
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 1) {
+      $('body').addClass("sticky");
+    } else {
+      $('body').removeClass("sticky");
+    }
+  });
+  // smooth scroll
+  $(".smooth-scroll").click(function() {
+    var target = $(this).attr("href"),
+      scrollTo = $(target).offset().top,
+      headerHeight = $('#Header').outerHeight();
+    console.log(headerHeight);
+    $('html, body').animate({ scrollTop: scrollTo - headerHeight });
+    $('#nav-icon1').trigger("click");
+    $("body").removeClass("menu-active");
+  });
+  //menu icon animation
+  $('#nav-icon1').click(function() {
+    $(this).toggleClass('open');
+    $("body").toggleClass("menu-active");
+  });
+  //menu link click
+});
+
 // modale
 $(document).ready(function() {
     $("#modale_open").click(function() {
@@ -10,4 +36,3 @@ $(document).ready(function() {
         $(".modale").fadeOut();
     });
 });
-
