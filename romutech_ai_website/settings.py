4b75a3cec894dd04b3a0bb6ca06964062c959b0b
Robin Muller <robin.muller.romutech@gmail.com>
2021-02-13 16:42:18 +0100

    chore: implementation of the database cache system

--- romutech_ai_website/settings.py
+++ romutech_ai_website/settings.py
@@ -85,6 +85,13 @@ DATABASES = {
     }
 }
 
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.db.DatabaseCache',
+        'LOCATION': 'caching',
+    }
+}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
